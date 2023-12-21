3538db818d087f2454f225ee3dc5b8917d9be996
Damandundel <damian.georgiev08@gmail.com>
2023-12-21 10:23:20 +0200

    Update Program.cs

--- 1. Offroad Challenge/1. Offroad Challenge/Program.cs
+++ 1. Offroad Challenge/1. Offroad Challenge/Program.cs
@@ -1,9 +1,45 @@
-﻿namespace _1._Offroad_Challenge;
-class Program
+﻿namespace offroad_challenge
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main()
+        {
+            int[] initialFuel = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] consumptionIndexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] neededFuel = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int altitudesCount = initialFuel.Length;
+
+            for (int i = 0; i < altitudesCount; i++)
+            {
+                int currentAltitudeFuel = initialFuel.Last() - consumptionIndexes.First();
+
+                if (currentAltitudeFuel >= neededFuel.First())
+                {
+                    Console.WriteLine($"John has reached: Altitude {i + 1}");
+
+                    initialFuel = initialFuel.Take(altitudesCount - i - 1).ToArray();
+                    consumptionIndexes = consumptionIndexes.Skip(1).ToArray();
+                    neededFuel = neededFuel.Skip(1).ToArray();
+                }
+                else
+                {
+                    Console.WriteLine($"John did not reach: Altitude {i + 1}");
+
+                    if (i > 0)
+                    {
+                        Console.WriteLine($"John failed to reach the top.\nReached altitudes: {string.Join(", ", Enumerable.Range(1, i))}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("John failed to reach the top.\nJohn didn't reach any altitude.");
+                    }
+
+                    return;
+                }
+            }
+
+            Console.WriteLine("John has reached all the altitudes and managed to reach the top!");
+        }
     }
 }
-
